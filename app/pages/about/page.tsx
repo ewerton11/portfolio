989f784ff55152b493ac3494f5482505c8ee7109
ewerton11 <ewertonreisc01@gmail.com>
2024-12-02 21:39:44 -0300

    new pages

---
+++ app/pages/about/page.tsx
@@ -0,0 +1,64 @@
+'use client'
+
+import Header from '@/app/components/header'
+import Image from 'next/image'
+
+export default function About() {
+  return (
+    <div className="w-full flex flex-col relative bg-primary-dark items-center min-h-screen">
+      <Header />
+      <div className="w-full h-screen flex justify-center" id="about">
+        <div className="w-[90%] md:w-[88%] h-full flex flex-col justify-center relative">
+          <div className="w-full h-full flex flex-col lg:justify-center text-sm lg:text-xl font-roboto">
+            <div className="lg:hidden w-full flex justify-center">
+              <div className="m-7 bg-white rounded-full flex justify-center item-center">
+                <Image
+                  src="/MyProfile.png"
+                  width={128}
+                  height={128}
+                  alt="profile"
+                  className="rounded-full w-32 h-32 object-cover"
+                />
+              </div>
+            </div>
+            <h1 className="text-gray-100 mt-7">(SOBRE)</h1>
+            <div className="w-full h-auto flex items-center mt-7 lg:mt-0">
+              <p className="flex-1">
+                Me chamo Ewerton e sou desenvolvedor fullstack. Atualmente,
+                tenho 22 anos e moro no Rio de Janeiro, Brasil. Sempre fui
+                apaixonado por tecnologias e sou curioso. Foi assim que conheci
+                a área de programação.
+                <br />
+                Aprendi os fundamentos de forma autodidata e, com o passar dos
+                anos, comecei a me aperfeiçoar com projetos e freelances. Hoje,
+                sigo nesse modelo, trabalhando como freelancer, ajudando pessoas
+                e me desenvolvendo cada vez mais.
+              </p>
+              <div className="hidden lg:block lg:w-auto">
+                <div className="bg-white rounded-full flex justify-center item-center">
+                  <Image
+                    src="/MyProfile.png"
+                    width={256}
+                    height={256}
+                    alt="profile"
+                    className="rounded-full w-64 h-64 object-cover"
+                  />
+                </div>
+              </div>
+            </div>
+            <div className="w-full flex justify-end">
+              <div className="w-64 flex justify-center mt-16 lg:mt-10">
+                <p className="text-base text-center text-gray-100">
+                  DESENVOLVEDOR FULL-STACK
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="absolute left-1/2 -translate-x-1/2 bottom-10 cursor-pointer">
+            {/* <p className="text-base font-bold font-roboto">SOBRE</p> */}
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
---
+++ app/pages/contact/page.tsx
@@ -0,0 +1,63 @@
+'use client'
+
+import Header from '@/app/components/header'
+import ArrowRightIcon from '@/public/ArrowRightIcon'
+import Link from 'next/link'
+
+export default function Contact() {
+  return (
+    <div className="w-full flex flex-col relative bg-primary-dark items-center min-h-screen">
+      <Header />
+      <div
+        className="flex flex-col items-center min-h-screen h-screen"
+        id="contact"
+      >
+        <div className="w-[90%] md:w-[88%] h-full flex items-center">
+          <div className="w-full h-2/5 sm:h-2/3 border rounded-3xl flex flex-col justify-center items-center">
+            <div className="w-[80%] h-[80%] flex flex-col">
+              <div className="w-full h-5/6 flex flex-col border-b font-roboto">
+                <h1 className="text-2xl sm:text-5xl font-normal">
+                  Vamos criar algo <br />
+                  incrível juntos.
+                </h1>
+                <div className="flex-1 flex justify-end items-center">
+                  <div className="w-20 sm:w-24 mr-5">
+                    <ArrowRightIcon />
+                  </div>
+                  <Link
+                    className="text-sm sm:text-xl"
+                    href="mailto:ewertonreisc01@gmail.com"
+                  >
+                    ENVIE-ME UM EMAIL
+                  </Link>
+                </div>
+              </div>
+              <div className="flex-1">
+                <ul className="flex h-full items-center text-sm font-medium sm:font-semibold font-roboto">
+                  <li className="p-2">
+                    <Link
+                      href="https://www.linkedin.com/in/ewerton11"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      LinkedIn
+                    </Link>
+                  </li>
+                  <li className="ml-5 p-2">
+                    <Link
+                      href="https://github.com/ewerton11"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      GitHub
+                    </Link>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
